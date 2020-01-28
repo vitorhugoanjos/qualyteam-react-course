4a49e56744198ef8ae4313003c4564b0d6b56262
Gabriel <gabriel@qualyteam.com.br>
2020-01-28 13:55:16 -0300

    Remove unnecessary component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.png";
-import { AddButton } from "./add-button";
 
-const Navbar = () => (
-  <nav className="navbar">
-    <Link to="/" className="navbar--logo-container">
-      <figure className="">
-        <img src={logo} alt="Uma imagem impressionante" />
-      </figure>
-    </Link>
-    <div className="navbar--center-container">
-      <input
-        role="search"
-        placeholder="Procure uma receita"
-        aria-labelledby="search"
-      />
-    </div>
-    <div className="navbar--right-container">
-      <AddButton />
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar--logo-container">
+        <figure className="">
+          <img src={logo} alt="Uma imagem impressionante" />
+        </figure>
+      </Link>
+      <div className="navbar--center-container">
+        <input
+          role="search"
+          placeholder="Procure uma receita"
+          aria-labelledby="search"
+        />
+      </div>
+      <div className="navbar--right-container">
+        <Link to="/food/create" className="button--plus">
+          +
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export { Navbar };
